60a014c3b9c3c353808cfa060cf9d4d54eb114e9
Watson-69 <morsecode453@gmail.com>
2026-02-18 11:18:30 +0530

    yurr

---
+++ CLab_Works/New folder/arrays.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+// int main(){
+    // int scope[5]={75,43} baki sara zero isme i.e score[0]=75,socre[1]=43
+    // int arr[5]={2,4,5,6,7};
+    // printf("%d",&arr[5]);
+    // int arr[]={2,4,6,8,12,16,18};
+    // int n=sizeof(arr)/sizeof(arr[0]);
+    // int score[63];
+    // for(int i=0,i<60;i++){
+    //     score[i]=-1;
+    //     printf("\n %d",&score[i]);
+    // }
+    // printf("%d \n",arr[0]);    
+    // printf("%d \n",arr[2]);
+    // printf("%d \n",arr[3]);
+    // printf("%d \n",arr[1]);
+    // for(int i=0;i<n;i++){
+    //     printf("%d ",arr[i]);
+    // }
+    // printf("\n%d\n",n);
+    //     return 0;
+// }
+// #define (array ka vairable name i.e Bound) 25 -means max value is 25 i.e int arr[Bound]
+int main(){
+    // for(i=0;i<5;i++){ for traverse in forward
+    //     // printf("%d \n",marks[i]);
+    // }
+    // for(i=4;i>=0;i--){
+        //printf("%d \n",marks[i]);
+    // }
+    // int score[63];
+    // for(int i=0;i<60;i++){
+    //     // score[i]=-1;
+    //     score[i]=i;
+    //     printf("\n %d",score[i]);
+    // }
+    // int bound[40];i;
+    // for(i=0;i<=99;i++){
+    // bound[i]=i;
+    // }
+    // int j,i,marks[5]={223,334,12,23,54,56,23,4,52,34};
+    // for(i=0;i<10;i++){
+    //     for(j=i+1;j<10;j++){
+    //         if(a[j]>a[i]){
+    //             temp=a[i];
+    //             a[i]=a[j];
+    //             a[j]=temp;
+    //         }
+    //     }
+    // }
+    // printf()
+    int n,i;
+    printf("Enter ")
+};
